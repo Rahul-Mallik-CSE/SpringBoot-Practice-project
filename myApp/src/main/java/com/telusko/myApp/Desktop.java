0d8d6fa059fa0fefd9c596e6f34425ac3e028833
Rahul Mallik <rmallik191242@bscse.uiu.ac.bd>
2026-03-09 23:18:00 +0600

    Add @Qualifier annotation to Desktop class for bean injection clarity

--- myApp/src/main/java/com/telusko/myApp/Desktop.java
+++ myApp/src/main/java/com/telusko/myApp/Desktop.java
@@ -1,8 +1,10 @@
 package com.telusko.myApp;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
+@Qualifier("laptop")  // this is used to specify which bean to inject when there are multiple beans of the same type
 public class Desktop implements Computer{
 
 
