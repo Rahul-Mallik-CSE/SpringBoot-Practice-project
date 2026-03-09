0266f4a9a542cfda465bf7f3771dd13cd7cc06fb
Rahul Mallik <rmallik191242@bscse.uiu.ac.bd>
2026-03-09 21:12:44 +0600

    add Laptop class and integrate it into Dev for project compilation

--- myApp/src/main/java/com/telusko/myApp/Dev.java
+++ myApp/src/main/java/com/telusko/myApp/Dev.java
@@ -5,7 +5,10 @@ import org.springframework.stereotype.Component;
 @Component
 public class Dev {
 
+    Laptop laptop = new Laptop();
+    
     public void build(){
+        laptop.compile();
         System.out.println("Working on Spring Boot Project");
     }
 }
---
+++ myApp/src/main/java/com/telusko/myApp/Laptop.java
@@ -0,0 +1,10 @@
+package com.telusko.myApp;
+
+
+public class Laptop {
+
+
+    public void compile(){
+        System.out.println("Compiling with 404 bugs");
+    }
+}
